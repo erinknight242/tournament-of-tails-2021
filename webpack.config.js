5b02363a692b2b7403a495ffcd4e40d8fdcace1a
Erin Knight <erinknight242@gmail.com>
2021-01-19 16:53:59 -0600

    Update packages to run locally

--- webpack.config.js
+++ webpack.config.js
@@ -19,10 +19,15 @@ module.exports = {
     rules: [
       {
         test: /\.jsx?$/,
-        use: [
-          'babel-loader',
-        ],
         exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
       },
       {
         test: /\.scss$/,
